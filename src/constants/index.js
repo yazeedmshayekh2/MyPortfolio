d7d84b0ec119b401436175b7924f00e21c18e7c4
Yazeed Mshayekh <mshayekhy17@gmail.com>
2024-06-05 18:29:42 +0300

    Portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -241,11 +241,6 @@ export const Volunteering = [
 
 export const socialLinks = [
     {
-        name: 'Contact',
-        iconUrl: contact,
-        link: '/contact',
-    },
-    {
         name: 'GitHub',
         iconUrl: github,
         link: 'https://github.com/yazeedmshayekh2',
@@ -253,7 +248,7 @@ export const socialLinks = [
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/yazeed-mshayekh-924aa9235/',
+        link: 'https://www.linkedin.com/in/yazeedmshayekh',
     }
 ];
 
