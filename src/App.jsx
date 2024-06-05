612fc38a211527dec8977a8afc47c05661f4d247
Yazeed Mshayekh <mshayekhy17@gmail.com>
2024-06-05 18:15:38 +0300

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
                   <Route path='/projects' element={<Projects />} />
                   <Route path='/contact' element={<Contact />} />
                 </Routes>
-                <Footer />
+                <Footer/>
               </>
             }
           />
